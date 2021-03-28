7f52cd21ea4757b98b3186844ace778facfb74f9
10micky10 <10miguel94@gmail.com>
2021-03-28 16:00:06 -0400

    init project spring maven gradle add database

---
+++ src/main/java/com/micromarket/MicroMarketApplication.java
@@ -0,0 +1,12 @@
+package com.micromarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroMarketApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MicroMarketApplication.class, args);
+  }
+}
---
+++ src/test/java/com/micromarket/MicroMarketApplicationTests.java
@@ -0,0 +1,12 @@
+package com.micromarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroMarketApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
